12590dfabc46efe59af0aba7b689c2c8839f1622
Slimakanzer <gleb.larochkin@gmail.com>
2020-10-20 21:00:16 +0300

    set vs keybinding for vscode

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+VSCODE_DIR=~/.vscode/extensions
 
 echo "Installing and setup vimrc ..." 
 mkdir -p ~/.vim && \
-        cp "$SOURCE_DIR/.vimrc" ~/ && \
-        cp "$SOURCE_DIR/.dracula.vim" ~/.vim/ && \
-        echo "Done!"
+        cp -i "$SOURCE_DIR/.vimrc" ~/ && \
+        cp -i "$SOURCE_DIR/.dracula.vim" ~/.vim/ && \
+        echo "Done vim!"
+
+echo "Installing and setup vscode extensions ..."
+mkdir -p "$VSCODE_DIR" && \
+        cp -ri $SOURCE_DIR/vscode/* "$VSCODE_DIR" && \
+        echo "Done vscode!"
